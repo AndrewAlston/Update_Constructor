d39947bb34b170ad5dfcea67587939906920844a
Andrew <aa@alstonnetworks.net>
2026-04-06 15:50:36 +0400

    Added support for adding MP BGP SR Policy NLRI

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include <asm-generic/types.h>
 #include <stdbool.h>
-#include <string.h>
 #include <arpa/inet.h>
+#include <string.h>
 #include <stdio.h>
 #include "bgp_attributes.h"
-
 
 const __u8 FLAG_UNSET = 0;
 const __u8 FLAG_EXTENDED = (__u8)1<<4;
@@ -30,6 +29,18 @@ struct bgp_header {
     __u16 length; /**< BGP Header length (Network Byte Order */
     __u8 type; /**< BGP Packet type */
 } __attribute__((__packed__));
+
+#ifndef IPV4_AFI
+#define IPV4_AFI 1
+#endif
+
+#ifndef IPV6_AFI
+#define IPV6_AFI 2
+#endif
+
+#ifndef SR_POLICY_SAFI
+#define SR_POLICY_SAFI 73
+#endif
 
 void dump_buffer(void *buffer,__u16 size)
 {
@@ -90,7 +101,7 @@ char *bgp_find_attribute(struct bgp_header *hdr, __u8 code) {
         attr_len_p = ptr+19+2;
     __u16 attr_len = htons(*(__u16*)attr_len_p);
     ptr = attr_len_p+2;
-    char *attr_end = attr_len_p+2+attr_len;
+    const char *attr_end = attr_len_p+2+attr_len;
     while (ptr != attr_end) {
         if (ptr[1] == code)
             return ptr;
@@ -99,6 +110,37 @@ char *bgp_find_attribute(struct bgp_header *hdr, __u8 code) {
             continue;
         }
         ptr += (3+ptr[2]);
+    }
+    return NULL;
+}
+
+char *bgp_find_mp_afi_safi(struct bgp_header *hdr, const __u16 afi, const __u8 safi) {
+    char *ptr = (void *)hdr;
+    char *attr_len_p = NULL;
+    __u16 withdrawn_len = *(__u16*)(ptr+19);
+    if (withdrawn_len != 0)
+        attr_len_p = (ptr+19+2+htons(withdrawn_len));
+    else
+        attr_len_p = ptr+21;
+    ptr = attr_len_p+2;
+    __u16 attr_len = ntohs(*(__u16*)attr_len_p);
+    const char *attr_end = attr_len_p+2+attr_len;
+    while (ptr != attr_end) {
+        if (ptr[1] == MP_BGP_ATTRIBUTE) {
+            if ((ptr[0] & FLAG_EXTENDED) == FLAG_EXTENDED) {
+                if (*(__u16*)&ptr[4] == htons(afi) && ptr[6] == safi) {
+                    return ptr;
+                }
+            }
+            if (*(__u16*)&ptr[3] == htons(afi) && ptr[5] == safi) {
+                return ptr;
+            }
+        }
+        if ((ptr[0] & FLAG_EXTENDED) == FLAG_EXTENDED) {
+            ptr+= (4+ntohs(*(__u16*)&ptr[2]));
+            continue;
+        }
+        ptr+=3+ptr[2];
     }
     return NULL;
 }
@@ -174,8 +216,10 @@ int add_asn(struct bgp_header *hdr, __u32 asn) {
     __u16 remaining_length = ntohs(hdr->length)-(data_ptr-(char*)hdr);
     if (attr_len == 0) {
         // we need to add at least 6 bytes here
-        memcpy(&data_ptr[6], data_ptr, remaining_length);
-        memset(data_ptr, 0, 6);
+        if (remaining_length > 0) {
+            memcpy(&data_ptr[6], data_ptr, remaining_length);
+            memset(data_ptr, 0, 6);
+        }
         data_ptr[0] = AS_PATH_SEQUENCE;
         data_ptr[1] = 1;
         memcpy(&data_ptr[2], &asn, 4);
@@ -217,6 +261,104 @@ int add_route_target4(struct bgp_header *hdr, const char *target, __u16 color) {
     return 0;
 }
 
+int add_mp_bgp_nlri(struct bgp_header *hdr, const __u16 afi, const __u8 safi, const char *nlri, const __u16 nlri_len) {
+    char *ptr = bgp_find_mp_afi_safi(hdr, afi, safi);
+    char *data_ptr = ptr;
+    if (!ptr) {
+        printf("Failed locating MP BGP Attribute with AFI %u and SAFI %u\n", afi, safi);
+        return -1;
+    }
+    bool use_extended = false;
+    __u16 attr_len;
+    if ((ptr[0] & FLAG_EXTENDED) == FLAG_EXTENDED) {
+        attr_len = htons(*(__u16*)&ptr[2]);
+        use_extended = true;
+        dump_buffer(data_ptr, attr_len+4);
+        data_ptr = ptr+attr_len+4;
+    } else {
+        attr_len = ptr[2];
+        data_ptr = ptr+attr_len+3;
+    }
+    __u16 remaining_length = ntohs(hdr->length)-(data_ptr-(char *)hdr);
+    if (remaining_length > 0) {
+        memcpy(&data_ptr[nlri_len], data_ptr, nlri_len);
+        memset(data_ptr, 0, nlri_len);
+    }
+    memcpy(data_ptr, nlri, nlri_len);
+    if (use_extended) {
+        *(__u16*)&ptr[2] = htons(attr_len+nlri_len);
+        increment_lengths(hdr, nlri_len);
+        return 0;
+    }
+    ptr[2] = attr_len+nlri_len;
+    increment_lengths(hdr, nlri_len);
+    return 0;
+}
+
+int add_sr_policy_nlri(struct bgp_header *hdr, const __u16 afi, const __u32 distinguisher,
+    const __u32 color, const char *endpoint) {
+    char *ptr = bgp_find_mp_afi_safi(hdr, afi, SR_POLICY_SAFI);
+    if (!ptr) {
+        printf("Failed locating MP BGP AFI/SAFI %u/%u\n", afi, SR_POLICY_SAFI);
+        return -1;
+    }
+    if (afi == IPV4_AFI) {
+        char buf[13] = {0};
+        buf[0] = 96;
+        *(__u32*)&buf[1] = htonl(distinguisher);
+        *(__u32*)&buf[5] = htonl(color);
+        __u32 *address = (__u32*)&buf[9];
+        if (inet_pton(AF_INET, endpoint, address) != 1)
+            return -1;
+        add_mp_bgp_nlri(hdr, afi, SR_POLICY_SAFI, buf, 13);
+        return 0;
+    }
+    if (afi == IPV6_AFI) {
+        char buf[25] = {0};
+        buf[0] = 192;
+        *(__u32*)&buf[1] = htonl(distinguisher);
+        *(__u32*)&buf[5] = htonl(color);
+        if (inet_pton(AF_INET6, endpoint, &buf[9]) != 1)
+            return -1;
+        add_mp_bgp_nlri(hdr, afi, SR_POLICY_SAFI, buf, 25);
+        return 0;
+    }
+    return -1;
+}
+
+int add_mp_bgp(struct bgp_header *hdr, __u16 afi, const __u8 safi, const char *next_hop) {
+    if (afi != IPV4_AFI && afi != IPV6_AFI) {
+        printf("AFI was %u, expected %u or %u\n", afi, IPV4_AFI, IPV6_AFI);
+        return -1;
+    }
+    char next_hop6[16];
+    __u32 next_hop4;
+    if (afi == IPV4_AFI) {
+        if (inet_pton(AF_INET, next_hop, &next_hop4) != 1) {
+            return -1;
+        }
+    } else {
+        if (inet_pton(AF_INET6, next_hop, next_hop6) != 1)
+            return -1;
+    }
+    add_attrib_hdr(hdr, FLAG_OPTIONAL, MP_BGP_ATTRIBUTE);
+    char value[128] = {0};
+    *(__u16*)&value[0] = htons(afi);
+    value[2] = safi;
+    if (afi == IPV4_AFI) {
+        printf("Adding for IPv4 AFI\n");
+        value[3] = 4;
+        memcpy(&value[4], &next_hop4, 4);
+        add_attr_value(hdr, value, 9);
+    } else if (afi == IPV6_AFI) {
+        printf("Adding for IPv6 AFI\n");
+        value[3] = 16;
+        memcpy(&value[4], next_hop6, 16);
+        add_attr_value(hdr, value, 21);
+    }
+    return 0;
+}
+
 int main() {
     char update[4096] = {0}; // As per RFC4271 BGP updates cannot exceed 4096 bytes
     struct bgp_header *hdr = construct_update_header(update);
@@ -226,9 +368,10 @@ int main() {
     add_as_path(hdr);
     add_nh_attribute(hdr, "10.10.10.10");
     add_local_pref_attribute(hdr, 100);
+    add_mp_bgp(hdr,IPV4_AFI, SR_POLICY_SAFI, "10.10.10.10");
+    add_sr_policy_nlri(hdr, IPV4_AFI, 10, 10, "10.10.10.10");
     add_route_target4(hdr, "10.20.30.1", 0);
     add_asn(hdr, 65001);
     add_asn(hdr, 65002);
     dump_buffer(hdr, ntohs(hdr->length));
 }
-
